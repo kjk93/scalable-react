12172f60d1747bae51b708468f41b70fb84f37d9
Kyle Kravette <kkravette@gmail.com>
2018-02-16 16:02:59 -0500

    Finished Loading Data with Saga (4)

---
+++ app/containers/NavigationContainer/actions.js
@@ -0,0 +1,31 @@
+/*
+ *
+ * NavigationContainer actions
+ *
+ */
+
+import { 
+  REQUEST_TOPICS,
+  REQUEST_TOPICS_FAILED,
+  REQUEST_TOPICS_SUCCEEDED
+} from "./constants";
+
+export function requestTopics() {
+  return {
+    type: REQUEST_TOPICS,
+  };
+}
+
+export function requestTopicsSucceeded(topics) {
+  return {
+    type: REQUEST_TOPICS_SUCCEEDED,
+    topics,
+  };
+}
+
+export function requestTopicsFailed(message) {
+  return {
+    type: REQUEST_TOPICS_FAILED,
+    message,
+  };
+}
